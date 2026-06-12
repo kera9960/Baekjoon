479af124805a758c2d875295cb146b5080165450
kera9960 <kera9960@gmail.com>
2026-06-12 09:41:38 +0900

    [level 1] Title: 햄버거 만들기, Time: 34.42 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i< ingredient.length; i++) {
+            
+            list.add(ingredient[i]);
+            int size = list.size();
+            if(list.size() >= 4) {
+                if(list.get(size - 4) == 1 &&
+                   list.get(size - 3) == 2 &&
+                   list.get(size - 2) == 3 &&
+                   list.get(size - 1) == 1 )
+                   {
+                       list.remove(list.size() - 1);
+                       list.remove(list.size() - 1);
+                       list.remove(list.size() - 1);
+                       list.remove(list.size() - 1);
+                       answer++;
+                   }
+            }
+        }
+        
+        return answer;
+    }
+}
